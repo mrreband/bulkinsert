4889227a7277e2c6cb07e749a2314aa9db7bbd2f
mrreband <mr@reband.org>
2019-03-07 21:54:34 -0600

    adding a BulkInsertRequest object to hold most of the logic, adding a new BatchLoader command line project to handle multiple requests.  MaxDOP and UseInputQueue settings are not in the request object -- an instance of that should only be concerned with processing one file

---
+++ BatchLoader/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Configuration;
+
+namespace BatchLoader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bulkLoadParameters = GetDefaultParameters(args);
+            var inputFolder = bulkLoadParameters["InputFolder"];
+            var copyLocal = Convert.ToBoolean(bulkLoadParameters["CopyLocal"]);
+            var targetConnectionString = ConfigurationManager.ConnectionStrings["TargetConnection"].ConnectionString;
+
+            //cap max degrees of parallelism between 1 and 8
+            var maxDOP = Convert.ToInt32(ConfigurationManager.AppSettings["MaxDOP"].ToString());
+            maxDOP = (maxDOP < 1) ? 1 : (maxDOP > 8) ? 8 : maxDOP;
+
+            var inputFilePaths = Directory.GetFiles(inputFolder).ToList();
+            ProcessFiles(inputFilePaths, bulkLoadParameters, targetConnectionString, maxDOP);
+
+            Console.WriteLine("done");
+        }
+
+        static void ProcessFiles(List<string> InputFilePaths, Dictionary<string, string> bulkLoadParameters, String targetConnectionString, int maxDOP)
+        {
+            Parallel.ForEach(
+                InputFilePaths,
+                new ParallelOptions { MaxDegreeOfParallelism = maxDOP },
+                inputFilePath =>
+                {
+                    try
+                    {
+                        bulkLoadParameters["InputFilePath"] = inputFilePath;
+                        var bulkLoadRequest = new BulkInsert.BulkInsertRequest(bulkLoadParameters, targetConnectionString);
+                        bulkLoadRequest.ProcessRequest();
+                    }
+                    catch (Exception ex)
+                    {
+                        var errLog = new StreamWriter("err.log", false);
+                        errLog.WriteLine(inputFilePath + ": " + ex.Message + " at " + ex.StackTrace);
+                        errLog.Close();
+                    }
+                }
+            );
+        }
+
+
+        static Dictionary<string, string> GetDefaultParameters(string[] args)
+        {
+            //load from config
+            var bulkLoadParameters = new Dictionary<string, string>();
+            foreach (var appSettingKey in ConfigurationManager.AppSettings.Keys)
+                bulkLoadParameters.Add(appSettingKey.ToString(), ConfigurationManager.AppSettings[appSettingKey.ToString()]);
+
+            //user overrides
+            foreach (string s in args)
+            {
+                var keyValuePair = s.Trim().Split('=');
+                if (keyValuePair.Count() != 2) throw new ArgumentException("Improper Argument Format (should be key=value): " + s);
+                if (!bulkLoadParameters.ContainsKey(keyValuePair[0])) throw new ArgumentException("Argument not supported: " + s);
+                Console.WriteLine(s);
+                bulkLoadParameters[keyValuePair[0]] = keyValuePair[1];
+            }
+
+            return bulkLoadParameters;
+        }
+
+    }
+
+}
---
+++ BulkInsert/BulkInsertRequest.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkInsert
+{
+    public class BulkInsertRequest
+    {
+        private string InputFilePath { get; set; }
+        private string Delimiter { get; set; }
+        private string TargetServer { get; set; }
+        private string TargetDatabase { get; set; }
+        private string TargetSchema { get; set; }
+        private string TargetTable { get; set; }
+        private bool UseHeader { get; set; }
+        private int HeaderRowsToSkip { get; set; }
+        private bool CopyLocal { get; set; }
+        private bool Overwrite { get; set; }
+        private bool Append { get; set; }
+        private int DefaultColumnWidth { get; set; }
+        private int BatchSize { get; set; }
+        private string Comments { get; set; }
+        private string SchemaPath { get; set; }
+        private string ColumnFilter { get; set; }
+        private char QuoteIdentifier { get; set; }
+        private char EscapeCharacter { get; set; }
+        private string NullValue { get; set; }
+        private string TargetConnectionString { get; set; }
+
+        public BulkInsertRequest(string inputFilePath, string delimiter, string targetServer, string targetDatabase, string targetSchema, string targetTable, bool useHeader, int headerRowsToSkip, bool copyLocal, bool overwrite, bool append, int defaultColumnWidth, 
+            int batchSize, string comments, string schemaPath, string columnFilter, char quoteIdentifier, char escapeCharacter, string nullValue)
+        {
+            this.InputFilePath = inputFilePath;
+            this.Delimiter = delimiter;
+            this.TargetServer = targetSchema;
+            this.TargetDatabase = targetDatabase;
+            this.TargetSchema = targetSchema;
+            this.TargetTable = targetTable;
+            this.UseHeader = useHeader;
+            this.HeaderRowsToSkip = headerRowsToSkip;
+            this.CopyLocal = copyLocal;
+            this.Overwrite = overwrite;
+            this.Append = append;
+            this.DefaultColumnWidth = defaultColumnWidth;
+            this.BatchSize = batchSize;
+            this.Comments = comments;
+            this.SchemaPath = schemaPath;
+            this.ColumnFilter = ColumnFilter;
+            this.QuoteIdentifier = quoteIdentifier;
+            this.EscapeCharacter = escapeCharacter;
+            this.NullValue = NullValue;
+        }
+
+        public BulkInsertRequest(Dictionary<string, string> bulkLoadParameters, string targetConnectionString)
+        {
+            this.InputFilePath = bulkLoadParameters["InputFilePath"];
+            this.Delimiter = bulkLoadParameters["Delimiter"];
+            this.TargetServer = bulkLoadParameters["TargetSchema"];
+            this.TargetDatabase = bulkLoadParameters["TargetDatabase"];
+            this.TargetSchema = bulkLoadParameters["TargetSchema"];
+            this.TargetTable = bulkLoadParameters["TargetTable"];
+            this.UseHeader = Convert.ToBoolean(bulkLoadParameters["UseHeader"]);
+            this.HeaderRowsToSkip = Convert.ToInt32(bulkLoadParameters["HeaderRowsToSkip"]);
+            this.CopyLocal = Convert.ToBoolean(bulkLoadParameters["CopyLocal"]);
+            this.Overwrite = Convert.ToBoolean(bulkLoadParameters["Overwrite"]);
+            this.Append = Convert.ToBoolean(bulkLoadParameters["Append"]);
+            this.DefaultColumnWidth = Convert.ToInt32(bulkLoadParameters["DefaultColumnWidth"]);
+            this.BatchSize = Convert.ToInt32(bulkLoadParameters["BatchSize"]);
+            this.Comments = bulkLoadParameters["Comments"];
+            this.SchemaPath = bulkLoadParameters["SchemaPath"];
+            this.ColumnFilter = bulkLoadParameters["ColumnFilter"];
+            this.QuoteIdentifier = bulkLoadParameters["QuoteIdentifier"].ToCharArray()[0];
+            this.EscapeCharacter = bulkLoadParameters["EscapeCharacter"].ToCharArray()[0];
+            this.NullValue = bulkLoadParameters["NullValue"];
+
+            this.TargetConnectionString = targetConnectionString;
+            SetTargetConnectionString(TargetConnectionString);
+        }
+
+        public bool ProcessRequest()
+        {
+            if (!File.Exists(InputFilePath))
+                throw new FileNotFoundException(string.Format("Input File {0} was not found", InputFilePath));
+
+            var fileExtension = Path.GetExtension(InputFilePath).ToLower().Replace(".", "");
+            if (fileExtension == "zip")
+            {
+                InputFilePath = UnzipFile(InputFilePath);
+                fileExtension = Path.GetExtension(InputFilePath).ToLower().Replace(".", "");
+            }
+            else if (!(fileExtension == "csv" || fileExtension == "xlsx" || fileExtension == "sas7bdat" || fileExtension == "tab" || fileExtension == "xml"))
+                throw new NotImplementedException("Only csv, xlsx, xml, sas7bdat files are supported");
+
+            var localFolderPath = GetValidAceDbFileName(Path.GetFileNameWithoutExtension(InputFilePath));
+
+            var localFilePath = InputFilePath;
+            if (CopyLocal)
+            {
+                if (!Directory.Exists(localFolderPath)) Directory.CreateDirectory(localFolderPath);
+                var inputFileName = GetValidAceDbFileName(Path.GetFileName(InputFilePath));
+                localFilePath = Path.Combine(localFolderPath, inputFileName);
+                CopyFileForReadWrite(InputFilePath, localFilePath);
+            }
+
+            var bl = BulkLoaderFactory.GetBulkLoader(fileExtension, localFilePath, Delimiter, TargetDatabase, TargetSchema, TargetTable, Convert.ToBoolean(UseHeader), Convert.ToInt32(HeaderRowsToSkip), Convert.ToBoolean(Overwrite), Convert.ToBoolean(Append), Convert.ToInt32(BatchSize), TargetConnectionString, Convert.ToInt32(DefaultColumnWidth), NullValue, Comments, SchemaPath, ColumnFilter, QuoteIdentifier, EscapeCharacter);
+
+            bl.Notifier += Notify;
+            bl.LoadToSql();
+
+            var schemaFile = Path.Combine(localFolderPath, "Schema.ini");
+            if (File.Exists(schemaFile)) File.Delete(schemaFile);
+
+            if (CopyLocal)
+            {
+                Console.WriteLine("Deleting " + localFilePath);
+                File.Delete(localFilePath);
+                if (!Directory.EnumerateFileSystemEntries(localFolderPath).Any()) Directory.Delete(localFolderPath);
+            }
+            return true;
+        }
+        private static void CopyFileForReadWrite(string sourceFile, string targetFile)
+        {
+            Console.WriteLine(string.Format("Copying {0} to {1} for ReadWrite", sourceFile, targetFile));
+            File.Copy(sourceFile, targetFile, true);
+            var attributes = File.GetAttributes(targetFile);
+            if ((attributes & FileAttributes.ReadOnly) == FileAttributes.ReadOnly)
+            {
+                attributes = RemoveAttribute(attributes, FileAttributes.ReadOnly);
+                File.SetAttributes(targetFile, attributes);
+            }
+        }
+
+        private static FileAttributes RemoveAttribute(FileAttributes attributes, FileAttributes attributesToRemove)
+        {
+            return attributes & ~attributesToRemove;
+        }
+
+        private void SetTargetConnectionString(String targetConnectionString)
+        {
+            if (TargetServer != "")
+            {
+                TargetConnectionString = System.Text.RegularExpressions.Regex.Replace(targetConnectionString, @"data source\=\w+", "Data Source=" + TargetServer, System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+            }
+        }
+
+        static string GetValidAceDbFileName(string fileName)
+        {
+            fileName = Path.GetFileNameWithoutExtension(fileName).Replace(".", "");
+            if (fileName.Length <= 64) return fileName + ".csv";
+            return fileName.Substring(0, 60) + ".csv";
+        }
+
+        static string UnzipFile(string zipFilePath)
+        {
+            var unzippedFilePath = "";
+            using (var zippedFiles = ZipFile.OpenRead(zipFilePath))
+            {
+                if (zippedFiles.Entries.Count != 1)
+                    throw new NotImplementedException("Expected only one file in " + zipFilePath);
+
+                var unzippedFileName = Path.GetFileName(zippedFiles.Entries[0].FullName);
+                unzippedFilePath = Path.Combine(Path.GetDirectoryName(zipFilePath), unzippedFileName);
+                if (File.Exists(unzippedFilePath)) File.Delete(unzippedFilePath);
+
+                Console.WriteLine("Unzipping " + unzippedFileName);
+                ZipFile.ExtractToDirectory(zipFilePath, Path.GetDirectoryName(zipFilePath));
+            }
+            return unzippedFilePath;
+        }
+
+                static void Notify(object sender, BulkInsertClass.NotifyEventArgs args)
+        {
+            Console.WriteLine(string.Format("{0}: {1}", args.TargetTable, args.Message));
+        }
+    }
+}
